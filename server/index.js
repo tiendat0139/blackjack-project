68904eca44ae2ed9b98823eaf82de82f13a84664
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-06-13 23:18:41 +0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mysql from 'mysql';
-//import posts from './routers/posts.js';
+import posts from './routers/posts.js';
 
 const app = express();
 const PORT = process.env.port || 5000;
@@ -11,15 +11,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
 
-//app.use('/posts',posts);
+app.use('/posts',posts);
 
-const dbConn = mysql.createConnection({
+export const dbConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'blackjack' //cần tạo database tên là blackjack trước khi chạy server
 });
-dbConn.connect();
+dbConnection.connect((err) => {
+    if (err) {
+        console.log('Error occured while connecting to database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database as id ' + dbConnection.threadId);
+});
 
 
 app.listen(PORT, () => {
