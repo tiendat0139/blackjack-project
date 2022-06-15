52ee6fe10b3cc0993c0c4210bb552f5ebc231dc6
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-06-15 09:48:10 +0700

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql';
+
+export const dbConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'blackjack' //cần tạo database tên là blackjack trước khi chạy server
+});
+dbConnection.connect((err) => {
+    if (err) {
+        console.log('Error occured while connecting to database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database as id ' + dbConnection.threadId);
+});
