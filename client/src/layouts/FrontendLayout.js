2cf75391cabd920a04bcf133e5ec9bffe026f7f7
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-06-18 10:56:46 +0700

    upload Mycasino

--- client/src/layouts/FrontendLayout.js
+++ client/src/layouts/FrontendLayout.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import TopScreen from "../components/TopScreen";
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import MyCasino from '../components/MyCasino';
 const FrontendLayout = () => {
     return (
         <div>
             <Navbar/>
             <Routes>
                 <Route path="/" element={<TopScreen />}></Route>
+                <Route path="/my-casino" element={<MyCasino/>}></Route>
             </Routes>
         </div>
-    )
+    );
 }
 export default FrontendLayout
