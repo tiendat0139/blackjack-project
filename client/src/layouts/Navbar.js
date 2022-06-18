7b8aaf421a2d4fd8d87ddcd4b6d658d6930fa737
tiendat0139 <dattien090909@gmail.com>
2022-06-18 17:23:27 +0700

    add dropdown to navbar

--- client/src/layouts/Navbar.js
+++ client/src/layouts/Navbar.js
@@ -6,9 +6,24 @@ const Navbar = () => {
         <div className="nav-bar">
            <span className="game-name">ブラックジャック王様</span>
            <div className="nav-option">
-                <Link to={'user'} className="nav-link"><i className="fa-solid fa-cart-shopping"></i></Link>
-                <Link to={'store'} className="nav-link"> <i className="fa-solid fa-circle-user"></i></Link>
-               
+                <Link to={'#'} className="nav-link"><i className="fa-solid fa-cart-shopping"></i></Link>
+                <Link to={'store'} className="nav-link nav-user"> 
+                    <i className="fa-solid fa-circle-user "></i>
+                    <i className="fa-solid fa-caret-down nav-icon_dropdown"></i>
+                    <div className="nav-dropdown">
+                        <ul className="nav-dropdown_list">
+                            <li >
+                                <Link to={'/my-casino'} className="nav-dropdown_item">My Casino</Link>
+                            </li>
+                            <li>
+                                <Link to={'/profile'} className="nav-dropdown_item">プロフィール</Link>
+                            </li>
+                            <li>
+                                <Link to={'/setting'} className="nav-dropdown_item">設定</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </Link>
            </div>
         </div>
 
