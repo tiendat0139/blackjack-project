9ed4648a77941ad175f324b4c2c73d95f8aeed3d
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-06-26 16:35:53 +0700

    update controller and index for readability

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require ('body-parser');
-const mysql = require('mysql');
 
-const usersRouter = require('./routes/usersRouter');
-
-require('dotenv').config();
+const myCasinoController = require('./controllers/MyCasinoController');
+const AuthController =  require('./controllers/AuthController');
+const dbConnection = require('./config/database');
 
 const app = express();
 const PORT = process.env.port || 5000;
@@ -14,41 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
 
+app.post('/login', AuthController); //login
+app.post('/register', AuthController); //register
 
-const dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'employeesystem',
-    port: process.env.DB_PORT || '3306'
-});
-
-app.get('/my-casino', (req, res) => {
-    dbConnection.query('SELECT * from users', 
-    (err, result) => {
-        if (err){
-            console.log(err);
-        } else {
-            console.log('Retrieved from database successfully! Code: 001');
-            res.send(result);
-        }
-    });
-});
-
-
-dbConnection.connect((err) => {
-    if (err) {
-        console.log('Error occured while connecting to database: ' + err.stack);
-        return;
-    }
-    console.log('Connected to database as id ' + dbConnection.threadId);
-});
-
+app.get('/my-casino', myCasinoController); //retrieve data from database to MyCasino screen
+app.post('/my-casino/upgrade', myCasinoController); //upgrade casino level and update the database
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
 });
 
-
 module.exports.app = app;
-module.exports.dbConnection = dbConnection;
