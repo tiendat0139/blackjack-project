3bf9dd1949bd6c81a1c80192a24402e6ce19ab78
TSFhacker <dungbui1110@gmail.com>
2022-07-02 12:51:00 +0700

    update

--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const dbConnectionInfo = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'blackjack', //cần tạo database tên là blackjack trước khi chạy server
-    port: process.env.DB_PORT || '3306'
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_DATABASE || "blackjack",
+  port: process.env.DB_PORT || "3306",
 };
 
 const dbConnection = mysql.createPool(
--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require ('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const myCasinoController = require('./controllers/MyCasinoController');
-const AuthController =  require('./controllers/AuthController');
-const dbConnection = require('./config/database');
-const mysql = require("mysql");
-
-// const usersRouter = require('./routes/usersRouter');
-
-require("dotenv").config();
+const myCasinoController = require("./controllers/MyCasinoController");
+const AuthController = require("./controllers/AuthController");
+const ItemController = require("./controllers/ItemController");
+const CategoryController = require("./controllers/CategoryController");
+const dbConnection = require("./config/database");
 
 const app = express();
 const PORT = process.env.port || 5000;
@@ -18,80 +15,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
-app.post('/login', AuthController); //login
-app.post('/register', AuthController); //register
+app.post("/login", AuthController); //login
+app.post("/register", AuthController); //register
 
-app.post('/my-casino', myCasinoController); //retrieve data from database to MyCasino screen
-app.post('/my-casino/upgrade', myCasinoController); //upgrade casino level and update the database
-const dbConnection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "blackjack",
-  port: process.env.DB_PORT || "3306",
-});
+app.post("/my-casino", myCasinoController); //retrieve data from database to MyCasino screen
+app.post("/my-casino/upgrade", myCasinoController); //upgrade casino level and update the database
 
-app.get("/my-casino", (req, res) => {
-  dbConnection.query("SELECT * from users", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Retrieved from database successfully! Code: 001");
-      res.send(result);
-    }
-  });
-});
-
-app.get("/store", (req, res) => {
-  dbConnection.query(
-    "SELECT * from items LEFT JOIN categories on items.category_id = categories.category_id",
-    (err, result) => {
-      if (err) {
-        console.log("Can't retrieve");
-      } else {
-        console.log("Retrieved from database successfully! Code: 001");
-        res.send(result);
-      }
-    }
-  );
-});
-
-app.get("/store/category/:id", (req, res) => {
-  dbConnection.query(
-    `SELECT * from items LEFT JOIN categories on items.category_id = categories.category_id WHERE items.category_id = ${req.params.id}`,
-    (err, result) => {
-      if (err) {
-        console.log("Can't retrieve");
-      } else {
-        console.log("Retrieved from database successfully! Code: 001");
-        res.send(result);
-      }
-    }
-  );
-});
-
-app.get("/category", (req, res) => {
-  dbConnection.query("SELECT * from categories", (err, result) => {
-    if (err) {
-      console.log("Can't retrieve");
-    } else {
-      console.log("Retrieved from database successfully! Code: 001");
-      res.send(result);
-    }
-  });
-});
-
-dbConnection.connect((err) => {
-  if (err) {
-    console.log("Error occured while connecting to database: " + err.stack);
-    return;
-  }
-  console.log("Connected to database as id " + dbConnection.threadId);
-});
+app.get("/store", ItemController);
+app.get("/store/category/:id", ItemController);
+app.get("/category", CategoryController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
 
 module.exports.app = app;
-module.exports.dbConnection = dbConnection;
