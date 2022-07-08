e43373a6ccc8c509aba87c15edf15dfd935c8f2d
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-07-09 06:48:50 +0700

    update controllers

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbConnection = require('../config/database');
+const pool = require('../config/databasePostgres');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
@@ -24,20 +25,33 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = req.body.email;
-    console.log(`A player has just signed in with ${username, password, email}!`);
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const name = req.body.name;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const casinoName = req.body.casinoName;
+    const casinoDesc = req.body.casinoDesc;
 
-    dbConnection.query('INSERT TO users (nickname, password, email) VALUES(?,?,?)',
+    const values = [username, password, email, name, gender, age, casinoName, casinoDesc];
+    console.log(values);
+    dbConnection.query('SELECT nickname from users where nickname = ?',[username],
     (err, result) => {
-        if (err){
-            console.log(err);
-            res.send({err: err});
+        if (result.length > 0){
+            res.send(false);
         } else {
-            console.log('Inserted a user into database successfully!');
+            dbConnection.query('INSERT INTO users (nickname, password, mail, name, gender, age, casino_name, casino_description) VALUES (?)', [values],
+            (err, result) => {
+                if (err){
+                    console.log(err);
+                } else {
+                    console.log('Inserted a user into database successfully!');
+                    res.send(true);
+                }
+            });
         }
-    })
+    }); 
 });
 
 module.exports = router;
--- server/controllers/PvEController.js
+++ server/controllers/PvEController.js
@@ -3,7 +3,7 @@ const dbConnection  = require('../config/database');
 
 const router = express.Router();
 
-router.post('/my-casino', (req, res) => {
+router.post('/pve', (req, res) => {
 
     const user_id = req.body.params.user_id;
 
@@ -14,27 +14,30 @@ router.post('/my-casino', (req, res) => {
             console.log(err);
         } 
         if (result.length > 0){
-            console.log('Retrieved data from database successfully! Code: myCasinoController');
+            console.log('A player has just started a game!');
             res.send(result);
         } else {
             res.send({message: 'User not found!'});
-            console.log('User not found! Error code: myCasinoController!');
+            console.log('User not found! Error code: pveController!');
         }
     });
 });
 
-router.post('/my-casino/upgrade', (req, res) => {
+router.post('/pve-update', (req, res) => {
+    const money = req.body.money;
+    const win = req.body.win;
+    const lose = req.body.lose;
+    const user_id = req.body.user_id;
 
-    dbConnection.query('UPDATE users SET level = ?', [level], 
+    dbConnection.query('UPDATE users set money = ?, win = ?, lose = ? where user_id = ?',
+    [money, win, lose, user_id],
     (err, result) => {
         if (err){
             res.send({err: err});
             console.log(err);
         }
-        if (result) {
-            console.log(result);
-        }
+        res.send(result);
     });
-});
+})
 
 module.exports = router;
