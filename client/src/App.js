f1e9e65723d3d0ca5cc33076d78be7d3cf24e429
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-07-09 06:55:27 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import "./styles.css";
-import BlackJack from "./BlackJack";
 import { Routes , Route} from 'react-router-dom'
 import FrontendLayout from '../src/layouts/FrontendLayout'
 
@@ -8,10 +8,9 @@ function App() {
     <div className="App">
         <Routes>
           <Route path='/*' exact element={<FrontendLayout/>}></Route>
-          <Route path='/pve' exact element={<BlackJack/>}></Route>
         </Routes>
     </div>
   );
 }
 
-export default App;
+ export default App;
