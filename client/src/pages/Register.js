12b3b28335f3ba8f59f2a165a2a311c7e5495dd5
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2022-07-16 09:20:18 +0700

    Add files via upload

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import { Link, Navigate } from 'react-router-dom';
 import '../css/Auth.css';
 import '../css/Tailwindcss.css';
+import AvatarUpload from './AvatarUpload';
 
 function Register({user}) {
 
@@ -30,7 +31,11 @@ function Register({user}) {
     const [formErrors, setFormErrors] = useState(initialErrors);
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [isRegister, setIsRegister] = useState(false);
-    const [successMessage, setSuccessMessage] = useState('');
+    const [message, setMessage] = useState('');
+    const [avatar, setAvatar] = useState({
+        selectedFileImg: null,
+        isUploaded: false
+    });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -38,6 +43,7 @@ function Register({user}) {
             return { ...prev, [name]: value };
         });
         setFormErrors(initialErrors);
+        setMessage('');
     }
 
     const handleSubmit = (e) => {
@@ -53,14 +59,14 @@ function Register({user}) {
                 gender: formValues.gender,
                 age: formValues.age,
                 casinoName: formValues.casinoName,
-                casinoDesc: formValues.casinoDesc
+                casinoDesc: formValues.casinoDesc,
+                avatar: avatar.selectedFileImg
             }).then((response) => {
                 if(response.data){
                     setIsRegister(true);
                 } else {
                     setIsRegister(false);
-                    // setSuccessMessage('ユーザー名　「' + formValues.username + '」はもう保存している。');
-                    alert('ユーザー名「' + formValues.username + '」はもう存在している。');
+                    setMessage('ユーザー名「' + formValues.username + '」はもう保存しています。');
                 }
             });
         }
@@ -69,35 +75,58 @@ function Register({user}) {
     const validate = (values) => {
         const errors = {};
         const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const regexName = /^[a-zA-Z\s]*$/;
+        const regexUsername = /^[a-zA-Z0-9]+$/;
+
+
         if (!values.username){
-            errors.username = "ユーザー名は必須入力！";
+            errors.username = "ユーザー名は必須入力";
         } else if (values.username.length > 45 || values.username.length < 6){
-            errors.username = "ユーザー名は6から45まで!";
+            errors.username = "ユーザー名の長さは6から45まで";
+        } else if (!regexUsername.test(values.username) && values.username.length > 0){
+            errors.username = "無効なユーザー名";
         }
+
+
         if (!values.password){
-            errors.password = "パスワードは必須入力！";
+            errors.password = "パスワードは必須入力";
         } else if (values.password.length < 5 || values.password.length > 31){
-            errors.password = "パスワードの長さは5から31まで！";
+            errors.password = "パスワードの長さは5から31まで";
         } else if (!values.confirmPassword){
-            errors.password = "パスワード確認は必須入力！";
+            errors.password = "パスワード確認は必須入力";
         } else if (values.confirmPassword !== values.password){
-            errors.password = "パスワードとパスワード確認は違います！";
+            errors.password = "パスワードとパスワード確認は違っています";
         }
+
+
         if (!values.name){
-            errors.name = "氏名は必須入力！";
+            errors.name = "氏名は必須入力";
         } else if (values.name.length > 30 || values.name.length < 4){
-            errors.name = "氏名は４から３０まで！";
-        }
+            errors.name = "氏名の長さは４から３０まで";
+        } else if (!regexName.test(values.name) && values.name.length > 0){
+            errors.name = "無効な氏名";
+        };
+
+
         if (!values.casinoName){
-            errors.casinoName = "カジノ名前は必須入力！";
+            errors.casinoName = "カジノ名前は必須入力";
         } else if (values.casinoName.length > 10 || values.casinoName.length < 4){
-            errors.casinoName = "カジノ名前は４から１０まで！";
+            errors.casinoName = "カジノ名前は４から１０まで";
         }
+
+
         if (!regexEmail.test(values.email) && values.email.length > 0){
-            errors.email = "メールアドレスはinvalid!";
+            errors.email = "無効なメールアドレス";
         }
+
         return errors;
     }
+
+
+    const getAvatar = (state) => {
+        setAvatar(state);
+    }
+
 
     if (isRegister){
         return <Navigate to={"/login"} replace/>;
@@ -107,45 +136,58 @@ function Register({user}) {
         <div id="form-register">
             <div className='register-card'>
                 <div id="form-title" className="mb-4">
-                        サインアップ
+                    サインアップ
                 </div>
+                { message && <p className='text-xl text-red-600'>{message}</p>}
                 <div className='register-two-div'>
                     <div className="register-div">
-                        { successMessage && <p className='text-xl text-red-500'>{successMessage}</p>}
-                        { formErrors.username && <p className='text-sm text-red-500'>{formErrors.username}</p>}
+                        
+                        { formErrors.username && <p className='text-sm text-red-600'>{formErrors.username}</p>}
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='ユーザー名' onChange={handleChange} name="username"/>
                         </div>
-                        { formErrors.name && <p className='text-sm text-red-500'>{formErrors.name}</p>}
+
+                        { formErrors.name && <p className='text-sm text-red-600'>{formErrors.name}</p>}
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='氏名' onChange={handleChange} name="name"/>
                         </div>
-                        {formErrors.email && <p className='text-sm text-red-500'>{formErrors.email}</p>}
+
+                        {formErrors.email && <p className='text-sm text-red-600'>{formErrors.email}</p>}
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='メールアドレス' onChange={handleChange} name="email"/>
                         </div>
-                        {formErrors.password && <p className='text-sm text-red-500'>{formErrors.password}</p>}
+
+                        {formErrors.password && <p className='text-sm text-red-600'>{formErrors.password}</p>}
                         <div className="mb-3 auth-input-label">
                             <input type="password" placeholder="パスワード" onChange={handleChange} name="password"/>
                         </div>
+
                         <div className="mb-3 auth-input-label">
                             <input type="password" placeholder="パスワード確認" onChange={handleChange} name="confirmPassword"/>
                         </div>
                     </div>
 
                     <div className='register-div'>
+                
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='性別' onChange={handleChange} name="gender"/>
                         </div>
+
                         <div className="mb-3 auth-input-label">
                             <input type="number" placeholder='年齢' onChange={handleChange} name="age"/>
                         </div>
-                        {formErrors.casinoName && <p className='text-sm text-red-500'>{formErrors.casinoName}</p>}
+
+                        {formErrors.casinoName && <p className='text-sm text-red-600'>{formErrors.casinoName}</p>}
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='カジノ名前' onChange={handleChange} name="casinoName"/>
                         </div>
+
                         <div className="mb-3 auth-input-label">
                             <input type="text" placeholder='カジノ説明' onChange={handleChange} name="casinoDesc"/>
+                        </div>
+
+                        <div>
+                            <AvatarUpload handleSubmit={getAvatar}/>
                         </div>
                         
                     </div>
